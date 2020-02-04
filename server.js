8388718d6959e63d5f3253d1767ec04773475b13
Luis Joel Caballero <luijo@outlook.com>
2020-02-03 19:39:47 -0600

    Added process.env to PORT so app now works on Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -18,5 +19,6 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller');
 app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT)
+});
