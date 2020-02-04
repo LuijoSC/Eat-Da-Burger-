8429e48cc668004656a4ade2f3007e2dc40a2ca7
Luis Joel Caballero <luijo@outlook.com>
2020-02-03 19:33:51 -0600

    App fully working

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Monster Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Kangreburger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Little Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Hawaian Burger', TRUE);
